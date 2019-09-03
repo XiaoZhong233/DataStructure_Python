91f21ca8880fafafba6987380ae19f7f39630fc4
钟浩伟 <610596547@qq.com>
2019-09-03 19:04:05 +0800

    :sunny:顺序查找

--- search/seq.py
+++ search/seq.py
@@ -1,11 +1,12 @@
-def search_seq(data,key):
-	data[0] = key
-	for i in range(len(data)-1,0,-1):
-		if data[i] == data[0]:
-			return i 
-		pass
+def search_seq(data, key):
+    data[0] = key
+    for i in range(len(data) - 1, 0, -1):
+        if data[i] == data[0]:
+            return i
+        pass
+
 
 if __name__ == '__main__':
-	data = [1,3,4,5]
-	print(search_seq(data,4))
-	pass
+    data = [1, 3, 4, 5]
+    print(search_seq(data, 4))
+    pass
