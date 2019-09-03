4268d00271e29b661e643369943288d4aab5f6fa
钟浩伟 <610596547@qq.com>
2019-09-03 19:01:20 +0800

    :sunny:无向网（图）邻接表实现

--- graph/GraphAL.py
+++ graph/GraphAL.py
@@ -248,19 +248,3 @@ class GraphAL:
             pathstrs += GraphAL.printPath(path) + "\n"
         return pathstrs
         pass
-
-# g = None
-# data = {
-#         "A": {"B": 5, "C": 1},
-#         "B": {"A": 5, "C": 2, "D": 1},
-#         "C": {"A": 1, "B": 2, "D": 4, "E": 8},
-#         "D": {"B": 1, "C": 4, "E": 3, "F": 6},
-#         "E": {"C": 8, "D": 3},
-#         "F": {"D": 6},
-# }
-# g = GraphAL(graph=data)
-# g.add_vertex("G")
-# g.add_edge("G", "A", 10)
-# print(g)
-# print(g.get_edge('A', 'G'))
-# print(g.out_edge('D'))
