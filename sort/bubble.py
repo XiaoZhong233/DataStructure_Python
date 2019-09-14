9ead703ee50b92f695c6f08dbbaa723fa3b74610
钟浩伟 <610596547@qq.com>
2019-09-14 20:02:54 +0800

    :sunny:冒泡排序改进

--- sort/bubble.py
+++ sort/bubble.py
@@ -30,6 +30,22 @@ def bubbleSort1(arr):
         bubble1(arr, i)
 
 
+# 改进版的冒泡排序，设置标志位标志是否发生交换，未发生交换则排序结束
+def bubble2(arr, n):
+    flag = False
+    for i in range(n - 1):
+        if arr[i] > arr[i + 1]:
+            swap(arr, i, i + 1)
+            flag = True
+    return flag
+
+
+def bubbleSort2(arr):
+    for i in range(len(arr), 0, -1):
+        if not bubble2(arr, i):
+            break
+
+
 if __name__ == '__main__':
     data = [3, 4, 6, 2, 8]
     print("升序冒泡")
@@ -37,4 +53,7 @@ if __name__ == '__main__':
     print(data)
     print("降序冒泡")
     bubbleSort1(data)
+    print(data)
+    print("改进版冒泡")
+    bubbleSort2(data)
     print(data)
